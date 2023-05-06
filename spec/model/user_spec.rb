dd139e0879816ff1d287e2b7c553dc2cd1fcea48
mohamed saleh <mohamed20163858@gmail.com>
2023-05-06 16:17:38 +0300

    Create model validation and test model specs

---
+++ spec/model/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  # tests go here
+  subject { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+  it 'PostsCounter must be an integer greater than or equal to zero' do
+    subject.posts_counter = nil
+    expect(subject).to_not be_valid
+  end
+  it 'PostsCounter must be zero when intialized' do
+    expect(subject.posts_counter).to equal(0)
+  end
+end
